bdbfa77de84791edbd613f21c833b56eb84a9882
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2021-04-18 10:14:53 +0300

    begin

--- fibo_matrix.py
+++ fibo_matrix.py
@@ -1,1 +1,12 @@
-import cairo
+from PIL import Image, ImageDraw
+
+
+# create an image
+out = Image.new("RGB", (1024, 1024), (255, 255, 255))
+
+draw = ImageDraw.Draw(out)
+draw.line((0, 0) + out.size, fill=128)
+draw.line((0, out.size[1], out.size[0], 0), fill=128)
+
+# write to stdout
+out.save("test.png")
